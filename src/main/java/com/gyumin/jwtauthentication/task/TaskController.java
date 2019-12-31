bdd19fb576887d507fb0f5245abb846b60872312
Gyumin Kim <starkying@gmail.com>
2019-12-31 20:23:17 +0900

    Add Task classes, modify sourceCompatibility to 11

---
+++ src/main/java/com/gyumin/jwtauthentication/task/TaskController.java
@@ -0,0 +1,40 @@
+package com.gyumin.jwtauthentication.task;
+
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+	private TaskRepository taskRepository;
+
+	public TaskController(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+
+	@PostMapping
+	public void addTask(@RequestBody Task task) {
+		taskRepository.save(task);
+	}
+
+	@GetMapping
+	public List<Task> getTasks() {
+		return taskRepository.findAll();
+	}
+
+	@PutMapping("/{id}")
+	public void editTask(@PathVariable long id, @RequestBody Task task) {
+		Task existingTask = taskRepository.getOne(id);
+		Assert.notNull(existingTask, "Task not found");
+		existingTask.setDescription(task.getDescription());
+		taskRepository.save(existingTask);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable long id) {
+		taskRepository.deleteById(id);
+	}
+}
---
+++ src/main/java/com/gyumin/jwtauthentication/task/TaskRepository.java
@@ -0,0 +1,6 @@
+package com.gyumin.jwtauthentication.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
